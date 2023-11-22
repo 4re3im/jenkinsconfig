1891a9a4ac8325ebac81cb0e337b2eb5730982a0
4re3im <98874722+4re3im@users.noreply.github.com>
2023-11-22 14:14:43 +0800

    Create rpmcreation.sh

---
+++ rpmcreation.sh
@@ -0,0 +1,27 @@
+#!/bash/bin
+
+# Enable error-sensitive shell
+set -e
+
+if [ -d SOURCES ]; then
+    rm -r SOURCES
+    mkdir SOURCES
+fi
+cp -rp $WORKSPACE/SOURCE/. SOURCES
+find SOURCES -name .git | sed -e 's/^://' -e 's/$//' | xargs rm -rf
+
+if [ -d SPECS ]; then
+    rm -r SPECS
+    mkdir SPECS
+fi
+cp -rp $WORKSPACE/SPEC/. SPECS
+find SPECS -name .git | sed -e 's/^://' -e 's/$//' | xargs rm -rf
+'''
+
+sh '''
+#!/bin/bash
+
+# Enable error-sensitive shell
+set -e
+
+rpmbuild --define "_version ${VERSION}" --define "_release $BUILD_NUMBER" --define "_topdir $WORKSPACE" -bb SPECS/${PACKAGE_NAME}.spec
