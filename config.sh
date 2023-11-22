ec46b26e12c9bbce3c3a29e3a1f716c3d53c71d0
4re3im <98874722+4re3im@users.noreply.github.com>
2023-11-22 13:39:09 +0800

    Update and rename config.groovy to config.sh

---
+++ config.sh
@@ -0,0 +1,15 @@
+# Read branch and repo_url from Branch.txt
+branch=$(grep '^branch=' "${WORKSPACE}/jenkinsconfig/Branch.txt" | cut -d'=' -f2)
+repo_url=$(grep '^giturl=' "${WORKSPACE}/jenkinsconfig/Branch.txt" | cut -d'=' -f2)
+
+# Trim leading and trailing whitespaces
+branch=$(echo "$branch" | tr -d '[:space:]')
+repo_url=$(echo "$repo_url" | tr -d '[:space:]')
+
+# Set environment variables
+export env.BRANCH="$branch"
+export env.REPO_URL="$repo_url"
+
+# Print the values
+echo "BRANCH: ${env.BRANCH}"
+echo "REPO_URL: ${env.REPO_URL}"
