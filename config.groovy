bf884f06900fc67481579407fb3a81beb6124216
4re3im <98874722+4re3im@users.noreply.github.com>
2023-11-22 14:12:19 +0800

    Update config.groovy

--- config.groovy
+++ config.groovy
@@ -9,5 +9,18 @@ env.REPO_URL = repo_url
 // Print the values
 echo "BRANCH: ${env.BRANCH}"
 echo "REPO_URL: ${env.REPO_URL}"
+
+sshagent(['sshgithub']) {
+    git branch: env.BRANCH, credentialsId: 'sshgithub', url: env.REPO_URL
+}
+
+def version = sh(script: "cat ${WORKSPACE}/version.txt | grep '^version=' | cut -d'=' -f2", returnStdout: true).trim()
+def packageName = sh(script: "cat ${WORKSPACE}/version.txt | grep '^package=' | cut -d'=' -f2", returnStdout: true).trim()
+
+env.VERSION = version
+env.PACKAGE_NAME = packageName
+
+echo "Version: ${env.VERSION}"
+echo "Package: ${env.PACKAGE_NAME}"
 }
 return this
