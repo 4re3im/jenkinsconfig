6c0bb478fa657eed4839cbb2dedf119652fd5ad7
4re3im <98874722+4re3im@users.noreply.github.com>
2023-11-22 18:38:58 +0800

    Update rpmcreation.sh

--- rpmcreation.sh
+++ rpmcreation.sh
@@ -3,18 +3,21 @@
 # Enable error-sensitive shell
 set -e
 
+cd $WORKSPACE/application
 if [ -d SOURCES ]; then
     rm -r SOURCES
     mkdir SOURCES
 fi
-cp -rp $WORKSPACE/SOURCE/. SOURCES
+cp -rp SOURCE/. SOURCES
 find SOURCES -name .git | sed -e 's/^://' -e 's/$//' | xargs rm -rf
 
 if [ -d SPECS ]; then
     rm -r SPECS
     mkdir SPECS
 fi
-cp -rp $WORKSPACE/SPEC/. SPECS
+cp -rp SPEC/. SPECS
 find SPECS -name .git | sed -e 's/^://' -e 's/$//' | xargs rm -rf
 
-rpmbuild --define "_version ${VERSION}" --define "_release $BUILD_NUMBER" --define "_topdir $WORKSPACE" -bb SPECS/${PACKAGE_NAME}.spec
+rpmbuild --define "_version $VERSION" --define "_release $BUILD_NUMBER" --define "_topdir $WORKSPACE/application" -bb SPECS/${PACKAGE_NAME}.spec
+
+/usr/bin/createrepo_c $WORKSPACE/application/RPMS/noarch
