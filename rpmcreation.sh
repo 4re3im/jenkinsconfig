1298de15ee1492df7491f0b9d8573f57e589b70b
4re3im <98874722+4re3im@users.noreply.github.com>
2023-11-23 17:37:08 +0800

    Update rpmcreation.sh

--- rpmcreation.sh
+++ rpmcreation.sh
@@ -1,104 +1,23 @@
-pipeline {
-    agent none
-    
-    options {
-        skipDefaultCheckout()
-    }
+#!/bin/bash
 
-    stages {
-        /*
-        //This cleanup will only be used when label 'built-in' is used 
-        stage('Initial Clean Workspace') {
-            agent {
-                label "built-in"
-            }  
-            steps {
-                cleanWs()
-            }
-        }
-        */
+# Enable error-sensitive shell
+set -e
 
-        stage('Build Package') {
-            agent {
-                label "cloud-agent-1"
-            }  
-            steps {
-                script {
-                    // Create jenkinsconfig and application directory                
-                    sh '''
-                    mkdir -p "${WORKSPACE}/jenkinsconfig"
-                    mkdir -p "${WORKSPACE}/application"
-                    '''
+cd $WORKSPACE/application
+if [ -d SOURCES ]; then
+    rm -r SOURCES
+    mkdir SOURCES
+fi
+cp -rp SOURCE/. SOURCES
+find SOURCES -name .git | sed -e 's/^://' -e 's/$//' | xargs rm -rf
 
-                    // Checkout jenkins configuration files
-                    dir("${WORKSPACE}/jenkinsconfig") {
-                        sshagent(['cup-gitlab']) {
-                            git branch: 'master', credentialsId: 'cup-gitlab', url: 'git@cup-gitlab.cambridge.org:bnr-education/tng-go.git'
-                        }
-                    }
+if [ -d SPECS ]; then
+    rm -r SPECS
+    mkdir SPECS
+fi
+cp -rp SPEC/. SPECS
+find SPECS -name .git | sed -e 's/^://' -e 's/$//' | xargs rm -rf
 
-                    // Loading and executing the config script
-                    def configScript = load "${WORKSPACE}/jenkinsconfig/extract.groovy"
-                    configScript.Extract()
+rpmbuild --define "_version $VERSION" --define "_release $BUILD_NUMBER" --define "_topdir $WORKSPACE/application" -bb SPECS/${PACKAGE_NAME}.spec
 
-                    // Create RPM package
-                    sh """
-                    ls -la ${WORKSPACE}
-                    pwd
-                    chmod 755 jenkinsconfig/rpmcreation.sh
-                    ./jenkinsconfig/rpmcreation.sh
-                    """
-
-                    // Push to S3
-                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'backoffice-nonprod']]) {
-                        sh "aws s3 cp $WORKSPACE/application/RPMS/noarch/${PACKAGE_NAME}-${VERSION}-${BUILD_NUMBER}.noarch.rpm s3://bnr-jenkins/package-repository/${PACKAGE_NAME}-${VERSION}-${BUILD_NUMBER}.noarch.rpm --region eu-west-1"
-                    }
-                }
-            }
-        }
-        stage ('Execute createrepo') {
-            agent {
-                label 'cloud-agent-1'
-            }
-            steps {
-                script {
-                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'backoffice-nonprod']]) {
-                        sh "aws s3 sync s3://bnr-jenkins/package-repository/ $WORKSPACE --region eu-west-1"
-                        sh """/usr/bin/createrepo_c $WORKSPACE"""
-                        sh "aws s3 sync $WORKSPACE s3://bnr-jenkins/package-repository/"
-                    }
-                }        
-            }
-        }
-            
-
-        stage ('Deploy on Staging') {
-            agent {
-                label 'built-in'
-            }
-            steps {
-                input message: 'Approval to Deploy on Staging', submitter: 'Dom AWS Admins'
-            }
-        }
-
-        stage ('Staging') {
-            agent {
-                label 'cloud-agent-1'
-            }
-            steps {
-                script {
-                    withCredentials([sshUserPrivateKey(credentialsId: 'ec2-user', keyFileVariable: 'SSH_KEY')]) {
-                        def remoteIp = '3.252.135.121'
-                        sh """
-                        ssh -o StrictHostKeyChecking=no -l ec2-user -i \${SSH_KEY} $remoteIp 'whoami'
-                        ssh -l ec2-user -i \${SSH_KEY} $remoteIp 'sudo yum clean all'
-                        ssh -l ec2-user -i \${SSH_KEY} $remoteIp 'sudo yum check-update || :'
-                        ssh -l ec2-user -i \${SSH_KEY} $remoteIp 'sudo yum -y remove cup-tng-go'
-                        ssh -l ec2-user -i \${SSH_KEY} $remoteIp 'sudo yum -y install cup-tng-go-${BUILD_NUMBER}'
-                        """
-                    }
-                }
-            }
-        }
-    }
-}
+#/usr/bin/createrepo_c $WORKSPACE/application/RPMS/noarch
